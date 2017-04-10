3ce41aec8aeb23cbb1958745c81e0e40630d8148
Kyushik <kyushikmin@gmail.com>
2017-04-10 16:30:42 +0900

    Update the code with functions

--- Double_Deep_Q_Learning.py
+++ Double_Deep_Q_Learning.py
@@ -28,6 +28,16 @@ Num_start_training = 20000
 Num_training = 300000
 Num_update = 2000
 Num_batch = 32
+Num_test = 50000
+
+img_size = 80
+
+first_conv   = [8,8,3,32]
+second_conv  = [4,4,32,64]
+third_conv   = [3,3,64,128]
+first_dense  = [10*10*128, 1024]
+second_dense = [1024, 256]
+third_dense  = [256, Num_action]
 
 game_name = 'dot_test'
 
@@ -93,77 +103,71 @@ def assign_network_to_target():
 	sess.run(update_bfc2)
 	sess.run(update_bfc3)
 
+def resize_and_norm(observation):
+	observation_out = cv2.resize(observation, (img_size, img_size))
+	observation_out = (observation_out - (255.0 / 2)) / (255.0 / 2)
+	return observation_out 
+
 # Input 
-x_image = tf.placeholder(tf.float32, shape = [None, 80, 80, 3])
+x_image = tf.placeholder(tf.float32, shape = [None, img_size, img_size, 3])
 
 # Convolution variables 
-w_conv1 = weight_variable([8,8,3,32])
-b_conv1 = bias_variable([32])
+w_conv1 = weight_variable(first_conv)
+b_conv1 = bias_variable([first_conv[3]])
 
-w_conv2 = weight_variable([4,4,32,64])
-b_conv2 = bias_variable([64])
+w_conv2 = weight_variable(second_conv)
+b_conv2 = bias_variable([second_conv[3]])
 
-w_conv3 = weight_variable([3,3,64,128])
-b_conv3 = bias_variable([128])
+w_conv3 = weight_variable(third_conv)
+b_conv3 = bias_variable([third_conv[3]])
 
 # Densely connect layer variables 
-w_fc1 = weight_variable([10*10*128, 1024])
-b_fc1 = bias_variable([1024])
+w_fc1 = weight_variable(first_dense)
+b_fc1 = bias_variable([first_dense[1]])
 
-w_fc2 = weight_variable([1024, 256])
-b_fc2 = bias_variable([256])
+w_fc2 = weight_variable(second_dense)
+b_fc2 = bias_variable([second_dense[1]])
 
-w_fc3 = weight_variable([256, Num_action])
-b_fc3 = bias_variable([Num_action])
+w_fc3 = weight_variable(third_dense)
+b_fc3 = bias_variable([third_dense[1]])
 
-
-# 
+# Network
 h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1, 4) + b_conv1)
-# h_pool1 = max_pool_2x2(h_conv1)
-
 h_conv2 = tf.nn.relu(conv2d(h_conv1, w_conv2, 2) + b_conv2)
-# h_pool2 = max_pool_2x2(h_conv2)
-
 h_conv3 = tf.nn.relu(conv2d(h_conv2, w_conv3, 1) + b_conv3)
-# h_pool3 = max_pool_2x2(h_conv3)
 
-h_pool3_flat = tf.reshape(h_conv3, [-1, 10*10*128])
+h_pool3_flat = tf.reshape(h_conv3, [-1, first_dense[0]])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, w_fc1)+b_fc1)
 h_fc2 = tf.nn.relu(tf.matmul(h_fc1, w_fc2)+b_fc2)
 
 output = tf.matmul(h_fc2, w_fc3) + b_fc3
 
 # Convolution variables target
-w_conv1_target = weight_variable([8,8,3,32])
-b_conv1_target = bias_variable([32])
+w_conv1_target = weight_variable(first_conv)
+b_conv1_target = bias_variable([first_conv[3]])
 
-w_conv2_target = weight_variable([4,4,32,64])
-b_conv2_target = bias_variable([64])
+w_conv2_target = weight_variable(second_conv)
+b_conv2_target = bias_variable([second_conv[3]])
 
-w_conv3_target = weight_variable([3,3,64,128])
-b_conv3_target = bias_variable([128])
+w_conv3_target = weight_variable(third_conv)
+b_conv3_target = bias_variable([third_conv[3]])
 
 # Densely connect layer variables target
-w_fc1_target = weight_variable([10*10*128, 1024])
-b_fc1_target = bias_variable([1024])
+w_fc1_target = weight_variable(first_dense)
+b_fc1_target = bias_variable([first_dense[1]])
 
-w_fc2_target = weight_variable([1024, 256])
-b_fc2_target = bias_variable([256])
+w_fc2_target = weight_variable(second_dense)
+b_fc2_target = bias_variable([second_dense[1]])
 
-w_fc3_target = weight_variable([256, Num_action])
-b_fc3_target = bias_variable([Num_action])
+w_fc3_target = weight_variable(third_dense)
+b_fc3_target = bias_variable([third_dense[1]])
 
-# 
+# Target Network 
 h_conv1_target = tf.nn.relu(conv2d(x_image, w_conv1_target, 4) + b_conv1_target)
-# h_pool1_target = max_pool_2x2(h_conv1_target)
-
 h_conv2_target = tf.nn.relu(conv2d(h_conv1_target, w_conv2_target, 2) + b_conv2_target)
-# h_pool2_target = max_pool_2x2(h_conv2_target)
-
 h_conv3_target = tf.nn.relu(conv2d(h_conv2_target, w_conv3_target, 1) + b_conv3_target)
-# h_pool3_target = max_pool_2x2(h_conv3_target)
 
-h_pool3_flat_target = tf.reshape(h_conv3_target, [-1, 10*10*128])
+h_pool3_flat_target = tf.reshape(h_conv3_target, [-1, first_dense[0]])
 h_fc1_target = tf.nn.relu(tf.matmul(h_pool3_flat_target, w_fc1_target)+b_fc1_target)
 h_fc2_target = tf.nn.relu(tf.matmul(h_fc1_target, w_fc2_target)+b_fc2_target)
 
@@ -184,7 +188,6 @@ config.gpu_options.allow_growth = True
 sess = tf.InteractiveSession(config=config)
 init = tf.global_variables_initializer()
 sess.run(init)
-
 
 # Load the file if the saved file exists
 saver = tf.train.Saver()
@@ -208,16 +211,14 @@ episode = 0
 game_state = game.GameState()
 action = np.zeros([Num_action])
 observation, reward, terminal = game_state.frame_step(action)
-observation = cv2.resize(observation, (80, 80))
-observation = (observation - (255.0 / 2)) / (255.0 / 2)
+observation = resize_and_norm(observation)
 
 # Making replay memory
 for i in range(Num_start_training):
 	action = np.zeros([Num_action])
 	action[random.randint(0, Num_action - 1)] = 1.0
 	observation_next, reward, terminal = game_state.frame_step(action)
-	observation_next = cv2.resize(observation_next, (80, 80))
-	observation_next = (observation_next - (255.0 / 2)) / (255.0 / 2)
+	observation_next = resize_and_norm(observation_next)
 
 	Replay_memory.append([observation, action, reward, observation_next, terminal])
 	observation = observation_next
@@ -225,7 +226,7 @@ for i in range(Num_start_training):
 		print('step: ' + str(step) + ' / '  + 'state: ' + state)
 	step += 1
 
-plt.figure(2)
+plt.figure(1)
 plot_x = []
 plot_y = []
 while True:
@@ -240,17 +241,15 @@ while True:
 		if random.random() < Epsilon:
 			action = np.zeros([Num_action])
 			action[random.randint(0, Num_action - 1)] = 1
-			observation_next, reward, terminal = game_state.frame_step(action)	
-			observation_next = cv2.resize(observation_next, (80, 80))	
-			observation_next = (observation_next - (255.0 / 2)) / (255.0 / 2)
+			observation_next, reward, terminal = game_state.frame_step(action)
+			observation_next = resize_and_norm(observation_next)
 		else:
-			observation_feed = np.reshape(observation, (1, 80, 80, 3))
+			observation_feed = np.reshape(observation, (1, img_size, img_size, 3))
 			Q_value = output.eval(feed_dict={x_image: observation_feed})[0]
 			action = np.zeros([Num_action])
 			action[np.argmax(Q_value)] = 1
 			observation_next, reward, terminal = game_state.frame_step(action)
-			observation_next = cv2.resize(observation_next, (80, 80))
-			observation_next = (observation_next - (255.0 / 2)) / (255.0 / 2)
+			observation_next = resize_and_norm(observation_next)
 
 		# Save experience to the Replay memory 
 		Replay_memory.append([observation, action, reward, observation_next, terminal])	
@@ -296,12 +295,7 @@ while True:
 		if Epsilon > Final_epsilon:
 			Epsilon -= 1.0/Num_training
 		
-		if step == Num_start_training + Num_training:
-			plt.savefig('./Plot/' + ' DDQN ' + game_name + '.png')		
 
-			# Send a note to pushbullet 
-			p.pushNote(devices[0]["iden"], 'DDQN', 'DDQN is done')	
-	
 	if step > Num_start_training + Num_training:
 		# Testing
 		state = 'Testing'
@@ -309,15 +303,22 @@ while True:
 		action = np.zeros([Num_action])
 		action[np.argmax(Q_value)] = 1
 		observation_next, reward, terminal = game_state.frame_step(action)
-		observation_next = cv2.resize(observation_next, (80, 80))
-		observation_next = np.reshape(observation_next, (1, 80, 80, 3))
-		observation_next = (observation_next - (255.0 / 2)) / (255.0 / 2)
+		observation_next = resize_and_norm(observation_next)
+		observation_next = np.reshape(observation_next, (1, img_size, img_size, 3))
 
 		observation_feed = observation_next
 
+	if step == Num_start_training + Num_training + Num_test:
+		plt.savefig('./Plot/' + ' DDQN ' + game_name + '.png')		
+
+		# Send a note to pushbullet 
+		p.pushNote(devices[0]["iden"], 'DDQN', 'DDQN is done')
+		# Finish the Code 
+		break		
+	
 	step += 1
 	score += reward 
-	
+
 	if terminal == True:
 		print('step: ' + str(step) + ' / '  + 'state: ' + state  + ' / '  + 'epsilon: ' + str(Epsilon) + ' / '  + 'score: ' + str(score)) 
 
@@ -339,5 +340,3 @@ while True:
 
 		plot_x = []
 		plot_y = [] 
-
-
