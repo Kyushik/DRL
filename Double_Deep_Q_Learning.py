Fix an error when plotting the average reward

Double_Deep_Q_Learning.py
@@ -190,7 +190,8 @@ y_prediction = tf.placeholder(tf.float32, shape = [None])
 y_target = tf.reduce_sum(tf.multiply(output, action_target), reduction_indices = 1)
 Loss = tf.reduce_mean(tf.square(y_prediction - y_target))
 # train_step = tf.train.RMSPropOptimizer(Learning_rate).minimize(Loss)
-train_step = tf.train.AdamOptimizer(learning_rate = Learning_rate, epsilon = 1e-04).minimize(Loss)
+train_step = tf.train.AdamOptimizer(learning_rate = Learning_rate, epsilon = 1e-02).minimize(Loss)
+# train_step = tf.train.RMSPropOptimizer(learning_rate = Learning_rate, momentum = 0.95).minimize(Loss)
 
 # Initialize variables
 config = tf.ConfigProto()
@@ -329,7 +330,7 @@ while True:
 			assign_network_to_target()
 
 		# Get y_prediction 
-		#Double Q Learning part
+		####################################### Double Q Learning part #######################################
 		Q_list = output.eval(feed_dict = {x_image: observation_next_batch})
 		a_max = []
 		for i in range(Q_list.shape[0]):
@@ -340,8 +341,10 @@ while True:
 			if terminal_batch[i] == True:
 				y_batch.append(reward_batch[i])
 			else:
-				y_batch.append(reward_batch[i] + Gamma * np.max(Q_batch[i, a_max[i]]))
+				y_batch.append(reward_batch[i] + Gamma * Q_batch[i, a_max[i]])
 
+		######################################################################################################
+		
 		train_step.run(feed_dict = {action_target: action_batch, y_prediction: y_batch, x_image: observation_batch})
 
 	    # save progress every 10000 iterations
@@ -404,7 +407,7 @@ while True:
 		score = 0
 		episode += 1
 
-	if len(plot_x) == Num_plot_episode:
+	if len(plot_x) % Num_plot_episode == 0 and len(plot_x) != 0:
 		plt.xlabel('Episode')
 		plt.ylabel('Score')
 		plt.title('Double Deep Q Learning')

Duel_Deep_Q_Learning.py
@@ -240,7 +240,7 @@ y_prediction = tf.placeholder(tf.float32, shape = [None])
 y_target = tf.reduce_sum(tf.multiply(output, action_target), reduction_indices = 1)
 Loss = tf.reduce_mean(tf.square(y_prediction - y_target))
 # train_step = tf.train.RMSPropOptimizer(Learning_rate).minimize(Loss)
-train_step = tf.train.AdamOptimizer(learning_rate = Learning_rate, epsilon = 1e-04).minimize(Loss)
+train_step = tf.train.AdamOptimizer(learning_rate = Learning_rate, epsilon = 1e-02).minimize(Loss)
 
 # Initialize variables
 config = tf.ConfigProto()
@@ -459,7 +459,7 @@ while True:
 		episode += 1
 
 
-	if len(plot_x) == Num_plot_episode:
+	if len(plot_x) % Num_plot_episode == 0 and len(plot_x) != 0:
 		plt.xlabel('Episode')
 		plt.ylabel('Score')
 		plt.title('Duel Deep Q Learning')