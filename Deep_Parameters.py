8997b21d35a031a31e724b03d891cbdfd549ee50
Kyushik <kyushikmin@gmail.com>
2017-09-19 22:03:44 +0900

    Remove some untested games

--- Deep_Parameters.py
+++ Deep_Parameters.py
@@ -7,8 +7,7 @@ sys.path.append("Wrapped_Game/")
 # pong = 3
 # dot, dot_test = 4
 # tetris = 5
-import pong as game 
-import pong_test 
+import pong as game
 import dot  
 import dot_test  
 import tetris  
@@ -22,18 +21,18 @@ Final_epsilon = 0.1
 Num_action = game.Return_Num_Action()
 
 # 30000 / 29900
-Num_replay_memory = 35000
-Num_start_training = 34900
-Num_training = 300000
-Num_update = 3000 - 10
+Num_replay_memory = 20000
+Num_start_training = 20000-1
+Num_training = 100000
+Num_update = 1000 - 1
 Num_batch = 32
-Num_test = 100000
+Num_test = 10000
 Num_skipFrame = 4
 Is_skip = True
 Num_stackFrame = 4
 Num_colorChannel = 1
 
-Num_plot_episode = 50
+Num_plot_episode = 20
 Num_step_save = 50000
 
 img_size = 80
